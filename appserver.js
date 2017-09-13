1cbe1a1938ff70d22d211d19f98eda49f29b0cba
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-13 14:25:30 +0200

    Update appserver.js

--- appserver.js
+++ appserver.js
@@ -1,1 +1,25 @@
 
+const server = require('express')();
+var sqlite3 = require('sqlite3').verbose();
+
+var cron;
+
+ 
+var db = new sqlite3.Database('devices');
+	
+db.run('CREATE TABLE IF NOT EXISTS devices(token text, state int)');	
+
+server.get('/add', function (req, res) {
+  
+})
+
+server.listen(80, function () {
+  console.log('server started.');
+  cron = setIntervall(_fetchState,1000);
+});
+
+function _fetchState() {
+    let req = 
+
+}
+
