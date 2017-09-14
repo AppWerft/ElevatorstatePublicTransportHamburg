6a5dad27c08ab97775016b30488752c96ed0ef8e
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-14 16:24:46 +0200

    Update appserver.js

--- appserver.js
+++ appserver.js
@@ -3,8 +3,11 @@ const serverInstance = require('express')();
 const fetch = require('node-fetch');
 const devicesDB = require('./devicesdb');
 const elevatorStates = require('./elevatorstates'); 
+const GEOFOX_URL = 'https://geofox.hvv.de/data-service/rest/elevators/stations/',
+  FCM_ENDPOINT = 'https://fcm.googleapis.com/fcm/send',
+  FCM_APPID = '';
 
-var cron;
+
 devicesDB.init(); 
 	
 // a device has subscribed to service and sends token:
@@ -17,20 +20,49 @@ serverInstance.get('/remove', function (req, res) {
 })
 
 // start server on port 80 
-serverInstance.listen(80, function () {
+serverInstance.listen(8888, function () {
   console.log('server started.');
-  cron = setIntervall(_fetchState,1000);
+  _fetchState();
+  let cron = setInterval(_fetchState,60000);
 });
 
-// cronjob triggers this function to get states of all elevators
-function _fetchState() {
-    fetch('https://geofox.hvv.de/data-service/rest/elevators/stations/',{method:'GET'})
-    .then(function(res) {
-        elevatorStates.update(res.json());
-    }).then(function(json) {
-        console.log(json);
-    });
 
+
+let _sendNotifications = diffs => {
+    let deviceTokens = devicesDB.getAll();
+    if (!deviceTokens) {
+      console.log("nix zu tun. Niemand ist neugierig");
+      return;
+    }
+    let body = {
+      to : deviceTokens,
+      title : "Aufzug kaputt"
+    };
+    console.log(body);
+    let options = {
+      method : 'POST',
+      headers : {
+        'Content-Type': 'application/json',
+        'Authorization' : FCM_APPID
+      },
+      body: JSON.stringify(body) 
+    };
+    fetch(FCM_ENDPOINT,options)
+     .then().catch();
 }
-
+// cronjob triggers this function to get states of all elevators
+let _fetchState=() => {
+  fetch(GEOFOX_URL,{method:'GET'})
+  .then(response =>response.json().then(json => {
+    //console.log(json);
+    let diffs = elevatorStates.update(json);
+    if (diffs) {
+      _sendNotifications(diffs);
+    }
+  })
+    
+  ).catch(error => {
+    console.log(error);
+  });
+}
 // https://geofox.hvv.de/jsf/showElevatorStates.seam
