02ee4a1df4f752465d9a9770a5435d31ded669d5
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-14 16:25:45 +0200

    Update elevatorstates.js

--- elevatorstates.js
+++ elevatorstates.js
@@ -1,21 +1,24 @@
-let state = null;
+let lastState = {};
 
 module.exports = {
     update : function(json) {
-      if (!state) {
-        state= json;
-        return;
-      }
+      var newState = {};
       // extracting hash of all states
-        json.stations.forEach(function(s){
-            let id = s.gfxId;
-            Object.getOwnProperties(s).forEach(function(e));
-        })
-        
+      json.stations.forEach(function(s){
+      Object.getOwnPropertyNames(s.elevators).forEach(e=>{
+           newState[s.gfxId+"_"+e]=s.elevators[e].state;
+        });
+      })
+      let diffs = []; 
       // make diff
-        
-      // if some diff => send to all subscribers a notification   
-        
+      Object.getOwnPropertyNames(newState).forEach(e=>{
+          if (newState[e] != lastState[e]) {
+            diffs.push({ e : newState[e]});
+          }
+      });      
       // save new elevator state to state
+      lastState = Object.assign(lastState,newState);
+       // if some diff => send to all subscribers a notification   
+       return diffs.length ? diffs : null; 
     }
 };
