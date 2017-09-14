cdc5d4803768be37d3a4b4d1726b5ac20cb223c6
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-14 16:56:24 +0200

    Update devicesdb.js

--- devicesdb.js
+++ devicesdb.js
@@ -1,25 +1,44 @@
 
 
-
 var sqlite3 = require('sqlite3').verbose();
 
 module.exports =
  {
-	init : function() { 
-		let db = new sqlite3.Database('devices');
-		db.run('CREATE TABLE IF NOT EXISTS devices(token text, state int)');	
+	init : () => { 
+		let db = new sqlite3.Database('devices',sqlite3.OPEN_CREATE);
+		db.run('CREATE TABLE IF NOT EXISTS devices(token text)');	
 		db.close();
 	},
-	add : function(token) {
-    let db = new sqlite3.Database('devices');
-		db.run('INSERT INTO devices (token)');	
+	subscribeDevice : token => {
+    let db = new sqlite3.Database('devices',sqlite3.OPEN_READWRITE);
+		db.run('INSERT INTO devices VALUES(?)',[token],(err) => {
+			if (err) {
+				return console.log(err.message);
+			}
+		});	
 		db.close();
 	},
-	delete : function(token) {
-    let db = new sqlite3.Database('devices');
-		db.run('DELETE FROM devices WHERE token="'+token+'"');	
+	unsubscribeDevice : token => {
+    let db = new sqlite3.Database('devices',sqlite3.OPEN_READWRITE);
+		db.run('DELETE FROM devices WHERE token=?',token,(err) => {
+			if (err) {
+				return console.log(err.message);
+			}
+		});		
 		db.close();
 	},
-  getAll : function() {
+  getAll : () => {
+		let devices = [];
+		let db = new sqlite3.Database('devices',sqlite3.OPEN_READONLY);
+		db.serialize(() => {
+			db.each(`SELECT token FROM devices`, (err, row) => {
+				if (err) {
+					console.error(err.message);
+				}
+				devices.push(row.token);
+			});
+		});
+		db.close();
   }
 };
+
