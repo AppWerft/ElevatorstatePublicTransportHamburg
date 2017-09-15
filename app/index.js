ce63cd840cc0f907f31b7cf4aa485a90acf5f142
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-15 23:02:29 +0200

          

--- app/index.js
+++ app/index.js
@@ -26,32 +26,9 @@ serverInstance.listen(8888, function () {
 });
 
 Fetcher.start();
-Fetcher.on(data, _sendNotifications); 
+
+Fetcher.on(data, (diff)=> {
+  new require('notification')(diff);
+}); 
 
 
-async function _sendNotifications(diffs) {
-  if (!diffs) {
-    console.log("nix zu tun. Status unverändert");
-    return;
-  }
-  var deviceTokens = await Devices.getAll();
-  if (!deviceTokens || deviceTokens.length==0) {
-      console.log("nix zu tun. Niemand ist neugierig");
-      return;
-  }
-  var body = {
-      to : deviceTokens,
-      title : "Aufzug kaputt",
-      vibrate : true
-    };
-    var options = {
-      method : 'POST',
-      headers : {
-        'Content-Type': 'application/json',
-        'Authorization' : "key="  + FCM_APPID
-      },
-      body: JSON.stringify(body) 
-    };
-    fetch(FCM_ENDPOINT,options)
-       .then(res=>{}).catch((err=>{}));
-}
