4e4b9cd34a0eb926978222b60d8bb5d7f10cccc0
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-15 15:16:36 +0200

          

--- app/devicesdb.js
+++ app/devicesdb.js
@@ -1,7 +1,7 @@
 "use strict"
 
 var sqlite3 = require('sqlite3').verbose();
-const absDBPath = require('path').resolve(__dirname, "SubscribedDevices.sqlite");
+const DB = require('path').resolve(__dirname, "SubscribedDevices.sqlite");
 
 const onError= err => {
 	if (err) {
@@ -11,7 +11,7 @@ const onError= err => {
 
 module.exports = class {
 	constructor () { 
-		let db = new sqlite3.Database(absDBPath);
+		let db = new sqlite3.Database(DB);
 		db.run('CREATE TABLE IF NOT EXISTS devices(token text)');	
 		db.close(err => {
 			if (err) {
@@ -21,20 +21,20 @@ module.exports = class {
 	};
 	
 	subscribe(token)  {
-    	let db = new sqlite3.Database(absDBPath,sqlite3.OPEN_READWRITE);
+    	let db = new sqlite3.Database(DB,sqlite3.OPEN_READWRITE);
 		db.run('INSERT INTO devices VALUES (?)',[token],onError);	    
 		db.close();
 	};
 	
 	unsubscribe(token) {
-    	let db = new sqlite3.Database(absDBPath,sqlite3.OPEN_READWRITE);
+    	let db = new sqlite3.Database(DB,sqlite3.OPEN_READWRITE);
 		db.run('DELETE FROM devices WHERE token=?',token,onError);		
 		db.close();
 	};
 	
 	getAll() {
 		var devices = [];
-		let db = new sqlite3.Database(absDBPath);
+		let db = new sqlite3.Database(DB);
 		return new Promise((resolve, reject) => {
 			db.all(`SELECT token FROM devices`, (err, rows) => {
 				if (err) {
