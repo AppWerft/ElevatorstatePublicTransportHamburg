c392da2df483dbd46c4308ab8f2508c497cece58
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-09-23 10:49:39 +0200

    Update notification.js

--- app/notification.js
+++ app/notification.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const Devices = require('./devicesdb'),
-FCM_APPID = '1:595170299549:android:a76b4928bb5e657d';
+FCM_APPID = 'AAAAipLqDp0:APA91bEf6R_BcxjTDZZ3srbLc__jhxkdgt2yGQ49pJlRBBPNsMUDhx9pXiK2PwXioaq3QI9-MGc1oHG0zpsKq1ZqR8TuoavgvbECJfCT-3mDwpJrHKwFltmfzYmeKxqYeCmNpgIN63h-';
 
 module.exports = class {
    constructor (diffs) {
